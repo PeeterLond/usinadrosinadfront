a9f95f97b799161b12d248e60b406c097fbc79ab
Elery <viljaelery@gmail.com>
2023-09-14 11:44:46 +0300

    KK-18

--- src/views/DashBoardView.vue
+++ src/views/DashBoardView.vue
@@ -1,62 +1,69 @@
 <template>
 
-<div class="row">
-  <div class="col">
-
-    <userImage/>
-
-    <table>
-      <tbody>
+  <div class="row">
+    <div class="col">
+      <UserImage :image-data-base64="userContactInfo.imageData" class="rounded" ></UserImage>
+      <table>
+        <tbody>
+        <tr>
+          <td>Kasutajanimi</td>
+          <td>{{ userContactInfo.userUsername }}</td>
+        </tr>
+        <tr>
+          <td>Eesnimi</td>
+          <td>{{ userContactInfo.contactFirstName }}</td>
+        </tr>
+        <tr>
+          <td>Perekonnanimi</td>
+          <td>{{ userContactInfo.contactLastName }}</td>
+        </tr>
+        <tr>
+          <td>Maakond</td>
+          <td>{{ userContactInfo.countyName }}</td>
+        </tr>
+        <tr>
+          <td>Linn</td>
+          <td>{{ userContactInfo.cityName }}</td>
+        </tr>
+        <tr>
+          <td>Telefoni number</td>
+          <td>{{ userContactInfo.contactMobileNumber }}</td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td>{{ userContactInfo.contactEmail }}</td>
+        </tr>
+        </tbody>
+      </table>
       <tr>
-        <td>Kasutajanimi</td>
-        <td>?</td>
+        <div placeholder="Koristaja lühitutvustus" class="dashboard-introduction mt-3" >
+          {{ userContactInfo.contactIntroduction }}
+        </div>
       </tr>
-      <tr>
-        <td>Eesnimi</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Perekonnanimi</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Linn</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Telefoni number</td>
-        <td>?</td>
-      </tr>
-      <tr>
-        <td>Email</td>
-        <td>?</td>
-      </tr>
-      </tbody>
-    </table>
-    <textarea placeholder="Koristaja lühitutvustus" cols="50" rows="5"></textarea>
-    <div class="row text-center">
-      <div class="col col-3">
-        <button @click="navigateToUserView" type="submit" class="btn btn-dark m-3">Redigeeri andmeid</button>
+      <div class="row text-center">
+        <div class="col col-3 ">
+          <button @click="navigateToUserView" type="submit" class="btn btn-dark m-3 ">Redigeeri andmeid</button>
+        </div>
       </div>
+
     </div>
 
+    <div class="col">
+      <div class="row">
+        <h4>Kuulutused</h4>
+        <div class="dash-board-advertisements">
+        </div>
+        <div>
+          <router-link to="/advertisement">
+            <button type="submit" class="btn btn-dark mt-2">Lisa kuulutus</button>
+          </router-link>
+        </div>
+      </div>
+      <div class="row">
+        Tagasiside
+      </div>
+    </div>
   </div>
-
-  <div class="col">
-    <div class="row">
-      <h4>Kuulutused</h4>
-      <div class="dash-board-advertisements">
-      </div>
-      <div>
-        <router-link to="/advertisement"><button type="submit" class="btn btn-dark mt-2">Lisa kuulutus</button></router-link>
-      </div>
-    </div>
-    <div class="row">
-      Tagasiside
-    </div>
-  </div>
-</div>
-
 
 
 </template>
@@ -64,19 +71,46 @@
 <script>
 import UserImage from "@/views/UserImage.vue";
 import router from "@/router";
+import ImageInput from "@/components/ImageInput.vue";
 
 export default {
   name: 'DashBoardView',
-  components: {UserImage},
+  components: {ImageInput, UserImage},
   data() {
     return {
-      currentUserId: sessionStorage.getItem('userId')
+      currentUserId: sessionStorage.getItem('userId'),
+      userContactInfo: {
+        userUsername: '',
+        countyName: '',
+        cityName: '',
+        imageData: '',
+        contactFirstName: '',
+        contactLastName: '',
+        contactMobileNumber: '',
+        contactEmail: '',
+        contactIntroduction: ''
+      }
     }
   },
   methods: {
     navigateToUserView() {
       router.push({name: 'userRoute', query: {userId: this.currentUserId}})
-    }
+    },
+    getUserContactInfo() {
+      this.$http.get("/contact-show", {
+            params: {
+              userId: this.currentUserId,
+            }
+          }
+      ).then(response => {
+        this.userContactInfo = response.data
+      }).catch(error => {
+        router.push({name: 'errorRoute'})
+      })
+    },
+  },
+  beforeMount() {
+    this.getUserContactInfo()
   }
 }
 </script>

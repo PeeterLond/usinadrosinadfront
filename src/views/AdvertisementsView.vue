25fd7131cb69318fa53d86da14a350cbf568246e
Peeter Lond <Peeter.Lond@outlook.com>
2023-09-18 17:03:30 +0300

    KK-33 WIP added filtering and sorting options

--- src/views/AdvertisementsView.vue
+++ src/views/AdvertisementsView.vue
@@ -1,8 +1,39 @@
 <template>
   <div class="advertisements-master">
-    <Advertisement :advertisement-response="advertisementResponse"/>
+    <div class="advertisements-filter">
+      <div>
+        <CountyDropdown @event-update-selected-county-id="getAdvertisementsByCountyAndSetCountyId"></CountyDropdown>
+      </div>
+      <div>
+        <CityDropdown @event-update-selected-city-id="getAdventuresByCity" ref="cityDropdownRef"></CityDropdown>
+      </div>
+    </div>
+    <div class="advertisements-inner">
+      <div class="advertisements-sort">
+        <div class="sort-header">
+          Hind:
+        </div>
+        <div class="sort-arrow">
+          <font-awesome-icon :icon="['fas', 'arrow-down']" @click="filterPriceDescending" size="xl" style="color: #000000;" />
+        </div>
+        <div class="sort-arrow">
+          <font-awesome-icon :icon="['fas', 'arrow-up']" @click="filterPriceAscending" size="xl" style="color: #000000;" />
+        </div>
+        <div class="sort-header">
+          Uuemad:
+        </div>
+        <div class="sort-arrow">
+          <font-awesome-icon :icon="['fas', 'arrow-down']" @click="filterOldest" size="xl" style="color: #000000;" />
+        </div>
+        <div class="sort-arrow">
+          <font-awesome-icon :icon="['fas', 'arrow-up']" @click="filterNewest" size="xl" style="color: #000000;" />
+        </div>
+      </div>
+      <div class="advertisements-ads">
+        <Advertisement :advertisement-response="advertisementResponse"/>
+      </div>
+    </div >
   </div>
-
 </template>
 
 <script>
@@ -12,12 +43,15 @@ import router from "@/router";
 import {ADVERTISEMENTS_IMAGE} from "@/assets/script/ImageSizes";
 import Advertisement from "@/components/Advertisement.vue";
 import Modal from "@/components/modal/Modal.vue";
+import CountyDropdown from "@/components/CountyDropdown.vue";
+import CityDropdown from "@/components/CityDropdown.vue";
 
 export default {
   name: 'AdvertisementsView',
-  components: {Modal, Advertisement},
+  components: {CityDropdown, CountyDropdown, Modal, Advertisement},
   data() {
     return {
+      countyId: 0,
       advertisementResponse: [
         {
           advertisementId: 0,
@@ -46,6 +80,64 @@ export default {
     }
   },
   methods: {
+    getAdventuresByCity(cityId) {
+      if (cityId > 0) {
+        this.getAdvertisementsByCity(cityId);
+      } else {
+        this.getAdvertisementsByCounty()
+      }
+
+    },
+    getAdvertisementsByCity(cityId) {
+      this.$http.get("/advertisement-with-contact-by-city", {
+            params: {
+              cityId: cityId,
+            }
+          }
+      ).then(response => {
+        this.advertisementResponse = response.data
+      }).catch(error => {
+        router.push({name: 'errorRoute'})
+      })
+    },
+    
+    getAdvertisementsByCountyAndSetCountyId(countyId) {
+      this.countyId = countyId
+      if (countyId > 0) {
+        this.getAdvertisementsByCounty();
+      } else {
+        this.getAllAdvertisementsWithContactInfo()
+      }
+
+      this.$refs.cityDropdownRef.selectedCountyId = countyId;
+      this.$refs.cityDropdownRef.getCities()
+    },
+
+    getAdvertisementsByCounty() {
+      this.$http.get("/advertisement-with-contact-by-county", {
+            params: {
+              countyId: this.countyId,
+            }
+          }
+      ).then(response => {
+        this.advertisementResponse = response.data
+      }).catch(error => {
+        router.push({name: 'errorRoute'})
+      })
+    },
+    filterPriceAscending() {
+      this.advertisementResponse.sort((a, b) => a.advertisementPrice - b.advertisementPrice)
+    },
+    filterPriceDescending() {
+      this.advertisementResponse.sort((a, b) => b.advertisementPrice - a.advertisementPrice)
+    },
+    filterNewest() {
+      this.advertisementResponse.sort((a, b) => new Date(a.advertisementTime) - new Date(b.advertisementTime))
+    },
+    filterOldest() {
+      this.filterNewest()
+      this.advertisementResponse.reverse()
+    },
     getAllAdvertisementsWithContactInfo() {
       this.$http.get("/advertisement-with-contact")
           .then(response => {
